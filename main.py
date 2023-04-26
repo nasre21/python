840c5da8fc7cb5fbe521a46d1ad16fafa2a29449
nasre21 <nasou2001@gmail.com>
2023-04-26 09:45:12 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+# Este es un comentario
+
+print("hola phyton")
+print('hola phyton')
+
+
+
+# Consultar el tipo  de dato
+print(type("Soy un dato str"))
+
+
+
---
+++ var.py
@@ -0,0 +1,38 @@
+
+# variables
+
+
+first_name= "nasser"
+
+last_name = "oufellah"
+
+print(last_name)
+
+my_bool_variable = True
+
+print(my_bool_variable, last_name)
+
+my_var = str(first_name)
+
+print(my_var)
+print(type(my_var))
+
+# Concatenacion
+
+
+print(f"{my_var} {last_name}")
+# inputs
+
+
+name = input("nasou")
+gmail = input("oufallag@21gmail.com")
+print(name)
+print(gmail)
+
+
+# operadores
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)

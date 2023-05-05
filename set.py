7ead8c12ec1fb2a48b7e6e924e4b12ecda268bfc
nasre21 <nasou2001@gmail.com>
2023-05-04 23:36:48 -0700

    crear set

--- set.py
+++ set.py
@@ -3,3 +3,19 @@ black = set([1, 2, 2, 5, 3])
 
 print(black)
 # Resault {1, 2, 3, 5}
+
+#add
+nasser = {"reloj", "recard", "blanco", "negro", "blanco"}
+
+nasser.add("elemento")
+print(set(nasser))
+
+
+#update
+
+nasser.update(["nassou", "big"])
+print(nasser)
+
+oufallah = {"nassedine", "white"}
+final = nasser.union(oufallah)
+print(final)

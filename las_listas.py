18d94dc7656cf7ed802eee2c55713536163b2f33
nasre21 <nasou2001@gmail.com>
2023-05-17 10:00:34 +0200

    acabar las tuplas

---
+++ las_listas.py
@@ -0,0 +1,19 @@
+mi_lista = ["Maria", "Pepe", "Marta", "Antonio" ,5, True]*3
+
+mi_lista.extend(["lala", "nasser"])
+
+mi_lista.remove("Maria")
+
+mi_lista.pop()
+
+
+
+print(mi_lista[:])
+
+mi_lista2 = [1, 3]
+
+mi_lista3 = [2, 3]
+
+mi_lista4 = mi_lista3 + mi_lista2
+
+print(mi_lista4)
---
+++ primeras_functiones.py
@@ -0,0 +1,13 @@
+def suma(num1,num2):
+    
+       resultado = num1+num2
+      
+       return resultado
+
+almacena = suma(2,5)
+
+print(almacena)
+
+print(suma(1,3))
+
+print(suma(5,8))
